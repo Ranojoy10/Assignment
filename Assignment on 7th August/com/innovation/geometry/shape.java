9d5600a63591d835e2b95ffa73b3fbce75a727ec
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-09-07 22:38:29 +0530

    Add files via upload

---
+++ Assignment on 7th August/com/innovation/geometry/shape.java
@@ -0,0 +1,9 @@
+package com.innovation.geometry;
+
+import com.innovation.shapes.Circle;
+
+public class shape {
+	public static void main(String[] args) {
+		Circle circle1 = new Circle();
+	}
+}
---
+++ Assignment on 7th August/com/innovation/shapes/Area.java
@@ -0,0 +1,9 @@
+package com.innovation.shapes;
+
+public class Area {
+	public static void main(String[] args) {
+		Circle circle2 = new Circle(10.5F);
+		
+	}
+}
+
